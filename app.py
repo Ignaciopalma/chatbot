e1f4de8e5b1f344d10793d9a9e570fb35ddaf1b9
Ignacio Palma <ignaciopalma@Ignacios-MacBook-Pro.local>
2019-04-29 00:33:02 -0400

    clean webpack files before recompiling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask import send_from_directory
 
-static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 app = Flask(__name__)
 
 @app.route("/")
