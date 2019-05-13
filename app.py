7fa9a835ef3f94c5e821755e92539c7116bef4e6
Ignacio Palma <ignaciopalma@Ignacios-MacBook-Pro.local>
2019-05-13 12:55:24 -0400

    adding model

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import os
 from flask import Flask
 from flask import send_from_directory
+from keras.models import model_from_json
+import spacy
+import numpy as np
+
+nlp = spacy.load('en_core_web_lg')
+
+# load json and create model
+json_file = open('/Users/ignaciopalma/PycharmProjects/session_6/models/mlp-model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("/Users/ignaciopalma/PycharmProjects/session_6/models/mlp-model.h5")
+print("Loaded model from disk")
+
+user_message = nlp("hello world").vector
+print("shape: {}".format(user_message.shape))
+
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print('model compiled')
+
+prediction = loaded_model.predict_classes(np.array([user_message]))
+
+print("prediction {}".format(prediction))
 
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 app = Flask(__name__)
